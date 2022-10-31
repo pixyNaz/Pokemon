5c7766c7544e257c456ee0803ec4da23789a36ec
Nazira Tynalieva <naziratynalieva84@gmail.com>
2022-10-31 13:13:53 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar/Navbar";
+import HomePage from "./pages/HomePage/HomePage";
+import ContactPage from "./pages/ContactPage/ContactPage";
+import HelpPage from "./pages/HelpPage/HelpPage"
+import {Route, Routes } from "react-router-dom"
+import Footer from "./components/Footer/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+    return (
+    <>
+        <Navbar />
+        <div className="container">
+            <Routes>
+                <Route path="/" element={<HomePage />}/>
+                <Route path="/help" element={<HelpPage />}/>
+                <Route path="/contact" element={<ContactPage />}/>
+            </Routes>
+        </div>
+        <Footer />
+    </>
+    )
 }
 
 export default App;
